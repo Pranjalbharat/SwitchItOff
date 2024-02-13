2206d7219c6cde8272bdaffc82f929317deb62a5
Saloni <sgoel0024@gmail.com>
2024-02-13 22:01:55 +0530

    bed

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,7 @@
 import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
 
 class SwitchItOff extends FlameGame{
+ 
   
 }
--- lib/room/day/light_off.dart
+++ lib/room/day/light_off.dart
@@ -1,5 +1,16 @@
+import 'dart:async';
+
 import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/sprite.dart';
 
 class lightoff extends SpriteComponent{
   
+ @override
+  Future<FutureOr<void>> onLoad() async {
+    // TODO: implement onLoad
+    Image image = (await Flame.images.load('background_day.png')) as Image;
+  }
 }
